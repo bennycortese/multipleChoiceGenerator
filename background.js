c7fc5c8011d0a4a77cf56fddbb5ed708cf2175a2
benny <bennycortese@gmail.com>
2023-12-23 13:45:54 -0500

    Now able to fetch from endpoints/render data based on what I fetched, should be able to now set up the python endpoints and zoom from here

--- background.js
+++ background.js
@@ -14,3 +14,40 @@ chrome.action.onClicked.addListener((tab) => {
     });
   }
 });
+
+function fetchData() {
+    // Return the fetch promise
+    return fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Data fetched', data);
+        return data; // Resolve the promise with the data
+      })
+      .catch(error => {
+        console.error('Error fetching data', error);
+        throw error; // Re-throw the error to be handled in the caller function
+      });
+  }
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.url.includes("www.youtube.com/watch") && changeInfo.status === "complete") {
+      fetchData(); // Fetch the data
+    }
+  });
+  
+  // Listen for a message request from the content script
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === "fetchData") {
+      fetchData().then(data => {
+        sendResponse({ data: data }); // Send the data back to the content script
+      }).catch(error => {
+        sendResponse({ error: error.toString() }); // Send error information back if needed
+      });
+      return true; // Keep the messaging channel open for sendResponse
+    }
+  });
--- content.js
+++ content.js
@@ -1,66 +1,94 @@
 console.log("YouTube Extension activated on this page.");
 
-window.addEventListener('load', () => {
-  const videoPlayer = document.querySelector('.html5-video-player');
-  const video = document.querySelector('video');
-
-  if (videoPlayer && video) {
-    const overlay = document.createElement('div');
-    overlay.style.position = 'absolute';
-    overlay.style.zIndex = '1000';
-    overlay.style.top = '0';
-    overlay.style.left = '0';
-    overlay.style.width = '100%';
-    overlay.style.height = '100%';
-    overlay.style.display = 'flex';
-    overlay.style.justifyContent = 'center';
-    overlay.style.alignItems = 'center';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0.75)'; // Darker semi-transparent background
-
-    // Define the HTML content for the overlay
-    const htmlContent = `
-    <div style="width: 80%; max-width: 500px; background-color: white; padding: 20px; border-radius: 10px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
-      <h2 style="color: #333; font-size: 24px; text-align: center; margin-bottom: 20px;">1. What is the capital of France?</h2>
-      <div style="margin-bottom: 10px;">
-        <label style="cursor: pointer; display: block; padding: 10px; background-color: #f9f9f9; border-radius: 5px;">
-          <input type="radio" name="option" value="A" style="margin-right: 10px;"/> <span style="color: #333;"> A) London </span>
-        </label>
-      </div>
-      <div style="margin-bottom: 10px;">
-        <label style="cursor: pointer; display: block; padding: 10px; background-color: #f9f9f9; border-radius: 5px;">
-          <input type="radio" name="option" value="B" style="margin-right: 10px;"/> <span style="color: #333;"> B) Paris </span>
-        </label>
-      </div>
-      <div style="margin-bottom: 10px;">
-        <label style="cursor: pointer; display: block; padding: 10px; background-color: #f9f9f9; border-radius: 5px;">
-          <input type="radio" name="option" value="C" style="margin-right: 10px;"/> <span style="color: #333;"> C) Berlin </span>
-        </label>
-      </div>
-      <div style="margin-bottom: 20px;">
-        <label style="cursor: pointer; display: block; padding: 10px; background-color: #f9f9f9; border-radius: 5px;">
-          <input type="radio" name="option" value="D" style="margin-right: 10px;"/> <span style="color: #333;">D) Madrid</span>
-        </label>
-      </div>
-      <button style="width: 100%; padding: 10px 20px; background-color: #3490dc; color: white; border: none; border-radius: 5px; cursor: pointer;">Click to Play/Pause</button>
-    </div>
-    `;
-
-    // Set the innerHTML of the overlay
-    overlay.innerHTML = htmlContent;
-
-    // Append the overlay to the video player
-    videoPlayer.appendChild(overlay);
-
-    // Find the button in the overlay and add click event to play/pause video
-    const button = overlay.querySelector('button');
-    button.addEventListener('click', () => {
-      if (!video.paused) {
-        video.pause();
-        button.innerText = 'Click to Play';
-      } else {
-        video.play();
-        button.innerText = 'Click to Pause';
-      }
-    });
-  }
+chrome.runtime.sendMessage({message: "fetchData"}, response => {
+    if (response && response.data) {
+        console.log('Data received from background:', response.data);
+        // Call a function to render the content with the data
+        renderContent(response.data);
+    } else {
+        console.error('No data received');
+    }
 });
+
+function renderContent(data) {
+window.addEventListener('load', () => {
+    if(true)
+    {
+    const videoPlayer = document.querySelector('.html5-video-player');
+    const video = document.querySelector('video');
+    if (videoPlayer && video) {
+        const overlay = document.createElement('div');
+        overlay.style.position = 'absolute';
+        overlay.style.zIndex = '1000';
+        overlay.style.top = '0';
+        overlay.style.left = '0';
+        overlay.style.width = '100%';
+        overlay.style.height = '100%';
+        overlay.style.display = 'flex';
+        overlay.style.justifyContent = 'center';
+        overlay.style.alignItems = 'center';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.75)'; // Darker semi-transparent background
+
+        const safeTitle = escapeHTML(data['title']);
+        // Define the HTML content for the overlay
+        const htmlContent = `
+        <div style="width: 80%; max-width: 500px; background-color: white; padding: 20px; border-radius: 10px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
+        <h2 style="color: #333; font-size: 24px; text-align: center; margin-bottom: 20px;">1. ${safeTitle}</h2>
+        <div style="margin-bottom: 10px;">
+            <label style="cursor: pointer; display: block; padding: 10px; background-color: #f9f9f9; border-radius: 5px;">
+            <input type="radio" name="option" value="A" style="margin-right: 10px;"/> <span style="color: #333;"> A) London </span>
+            </label>
+        </div>
+        <div style="margin-bottom: 10px;">
+            <label style="cursor: pointer; display: block; padding: 10px; background-color: #f9f9f9; border-radius: 5px;">
+            <input type="radio" name="option" value="B" style="margin-right: 10px;"/> <span style="color: #333;"> B) Paris </span>
+            </label>
+        </div>
+        <div style="margin-bottom: 10px;">
+            <label style="cursor: pointer; display: block; padding: 10px; background-color: #f9f9f9; border-radius: 5px;">
+            <input type="radio" name="option" value="C" style="margin-right: 10px;"/> <span style="color: #333;"> C) Berlin </span>
+            </label>
+        </div>
+        <div style="margin-bottom: 20px;">
+            <label style="cursor: pointer; display: block; padding: 10px; background-color: #f9f9f9; border-radius: 5px;">
+            <input type="radio" name="option" value="D" style="margin-right: 10px;"/> <span style="color: #333;">D) Madrid</span>
+            </label>
+        </div>
+        <button style="width: 100%; padding: 10px 20px; background-color: #3490dc; color: white; border: none; border-radius: 5px; cursor: pointer;">Click to Play/Pause</button>
+        </div>
+        `;
+
+        // Set the innerHTML of the overlay
+        overlay.innerHTML = htmlContent;
+
+        // Append the overlay to the video player
+        videoPlayer.appendChild(overlay);
+
+        // Find the button in the overlay and add click event to play/pause video
+        const button = overlay.querySelector('button');
+        button.addEventListener('click', () => {
+        if (!video.paused) {
+            video.pause();
+            button.innerText = 'Click to Play';
+        } else {
+            video.play();
+            button.innerText = 'Click to Pause';
+        }
+        });
+    }
+    }
+    
+  
+});
+  }
+
+  function escapeHTML(str) {
+    return str.replace(/[&<>'"]/g, 
+        tag => ({
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            "'": '&#39;',
+            '"': '&quot;'
+        }[tag]));
+}
